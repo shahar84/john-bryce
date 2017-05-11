7085070fa1415d9a807541269afffe8a163bee9f
Shahar polak <shaharp@breezometer.com>
2017-05-11 22:04:18 +0300

    Added associative array

---
+++ php/pre/07_associative_array.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$person1 = array(
+    "name" => "Shahar",
+    "status" => "married",
+    "children" => 1
+);
+
+echo '<pre>', print_r($person1, true), '</pre>';
+
+
+$child = array(
+    "name" => "Yahli",
+    "status" => "Single",
+    "children" => 0
+);
+
+$person2 = array(
+    "name" => "Shahar",
+    "status" => "married",
+    "children" => $child
+);
+
+echo '<pre>', print_r($person1, true), '</pre>';
