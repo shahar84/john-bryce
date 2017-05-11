9bfa3da2da39b9d5e42fe3d73ba66450c6ebef83
Shahar polak <shaharp@breezometer.com>
2017-05-11 22:31:52 +0300

    Added person class

--- pre/person.php
+++ pre/person.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * OOP - Object Oriented Programing
  *
@@ -11,8 +12,20 @@
 
 class person
 {
+    var $name, $profession, $gender;
 
-    function __construct()
+    function __construct($name = '', $profession = '', $gender = '')
     {
+        $this->name = $name;
+        $this->profession = $profession;
+        $this->gender = $gender;
+    }
+
+    function hello()
+    {
+        return "Hi my name is {$this->name}, I am a {$this->gender}, and I work as {$this->profession}";
     }
 }
+
+$person = new person("Shahar", "Full Stack Developer", "Male");
+print_r($person->hello());
