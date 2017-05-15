643c73bc18204a4ca51875836b29779e3c904f44
Shahar polak <shaharp@breezometer.com>
2017-05-15 23:12:26 +0300

    Added Array excises

---
+++ php/exercises/01_array.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Array Exercise 01
+ *
+ * Create a PHP script which displays the capital and country name from the array $countries.
+ * - Sort the list by the capital of the country.
+ * - Pay attention to uppercase the first letter
+ *  Sample Output :
+ *  The capital of Netherlands is Amsterdam
+ *  The capital of Greece is Athens
+ *
+ * */
+
+$countries = ["italy" => "rome", "luxembourg" => "luxembourg", "belgium" => "brussels", "denmark" => "copenhagen", "finland" => "helsinki", "france" => "paris", "slovakia" => "bratislava", "slovenia" => "ljubljana", "germany" => "berlin", "greece" => "athens", "ireland" => "dublin", "netherlands" => "amsterdam", "portugal" => "lisbon", "spain" => "madrid", "sweden" => "stockholm", "united kingdom" => "london", "cyprus" => "nicosia", "lithuania" => "vilnius", "czech republic" => "prague", "estonia" => "tallin", "hungary" => "budapest", "latvia" => "riga", "malta" => "valetta", "austria" => "vienna", "poland" => "warsaw"];
+
+
