840b096707785c8a3c4bf050ce09c7c5d4d28d8f
Shahar polak <shaharp@breezometer.com>
2017-05-18 22:05:10 +0300

    Replaced fetch_assoc() with fetch_object() function - and display movies

--- php/database/index.php
+++ php/database/index.php
@@ -4,10 +4,20 @@ include "connection.php";
 $results = $mysqli->query("SELECT * FROM movies ORDER BY id");
 
 $movies = array();
-while ($row = $results->fetch_assoc()) {
+$output = "";
+
+
+// Pay attention that we have replaced fetch_assoc() with fetch_object() function
+while ($row = $results->fetch_object()) {
     $movies[] = $row;
+    $output .= "<div class='movie'>
+                    <h2>$row->name ($row->year) </h2>
+                    <p>$row->description</p>
+                    <img src='$row->image_url'>
+                </div>";
 }
 // Free the memory
 $results->free();
-print_r($movies);
+//print_r($movies);
+echo $output;
 
