f94ebafb3f271fbff622b8957357dc8412b636ad
Shahar polak <shaharp@breezometer.com>
2017-05-19 12:56:59 +0300

    Update KM to miles conversion

--- php/exercises/04_convertion_class.php
+++ php/exercises/04_convertion_class.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Write a class that can convert the following:
- * 1) KM to Miles - (number * 1.609);
- * 2) Miles to KM - (number * 0.621)
+ * 1) KM to Miles -  (number * 0.621)
+ * 2) Miles to KM - (number * 1.609);
  * 3) Kilos to Pounds - (number * 2.20462)
  * 4) Pounds to Kilos - (number * 0.453592)
  *
  */
 
-class Convector{
+class Converter{
 
 }
 
