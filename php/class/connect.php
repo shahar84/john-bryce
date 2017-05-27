4bcc0caa7291b87fdebd6e5d404298b7b8ba73a2
Shahar polak <shaharp@breezometer.com>
2017-05-27 17:00:43 +0300

    Remove query from connection

--- php/class/connect.php
+++ php/class/connect.php
@@ -10,14 +10,4 @@ $mysqli = new mysqli($host, $username, $password, $db_name);
 if ($mysqli->connect_error) {
     die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
-
-
-
-
-
-$results = $mysqli->query("SELECT * FROM movies");
-echo '<ul>';
-while ($movie = $results->fetch_object()) {
-    echo "<li>$movie->name ($movie->release_date)</li>";
-    }
-echo '</ul>';
+$mysqli->set_charset("utf8");
