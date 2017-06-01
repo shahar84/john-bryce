26b35dc4b6fe3d1cf7475d6a5628c2d0443a56dc
Shahar polak <shaharp@breezometer.com>
2017-06-01 22:51:46 +0300

    Added style.css to main admin page

--- php/movie_website/admin/functions.php
+++ php/movie_website/admin/functions.php
@@ -10,14 +10,17 @@ function body_wrapper($output, $title = "Admin Panel")
           content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <link rel='stylesheet' href='./css/bootstrap.css'>
+    <link rel='stylesheet' href='./css/style.css'>
     <script src='./script/jquery.js'></script>
     <title>$title</title>
 </head>
 <body>
+<main>
     <div class='container'>
         $output
     </div>
 </body>
+</main>
 </html>";
 }
 
