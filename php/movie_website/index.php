9cc436c036ab6c197cdc05de62ca172aa96f19d3
Shahar polak <shaharp@breezometer.com>
2017-06-01 23:28:02 +0300

    Added images to index page

--- php/movie_website/index.php
+++ php/movie_website/index.php
@@ -5,13 +5,18 @@ include "functions.php";
 $results = $mysqli->query("SELECT * FROM movies ORDER BY year DESC");
 
 $output = "";
+ while ($movie = $results->fetch_object()) {
 
-while ($movie = $results->fetch_object()) {
-    $output .= "<li><a href='movie_view.php?id=$movie->id' title='$movie->name page'>$movie->name</a></li>";
+    $thumb = str_replace('w640', 'w300', $movie->image_url);
+//    $thumb =$movie->image_url;
+    $output .= "<li><a href='movie_view.php?id=$movie->id' title='$movie->name page'>
+                <img src='$thumb' onerror='imgError(this);' alt='$movie->name'></a></li>";
 }
-$output = "<ol>$output</ol>";
+$output = "<div class='movies-index'> 
+                    <h1 class='headline'>The best movie website in the world!</h1>
+                    <ul>$output</ul>        
+            </div>";
 //echo $output;
-
 
 
 echo body_wrapper($output, 'Movies index page');
