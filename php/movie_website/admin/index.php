ff92a7337c9660cdce0a93cc846e8baaa5416ec7
Shahar polak <shaharp@breezometer.com>
2017-06-01 22:52:06 +0300

    Added title to admin movies page

--- php/movie_website/admin/index.php
+++ php/movie_website/admin/index.php
@@ -4,7 +4,7 @@ include "../connection.php";
 include "functions.php";
 $results = $mysqli->query("SELECT * FROM movies ORDER BY year DESC");
 
-$output = "";
+$output = "<h1>Movies Admin</h1>";
 
 while ($movie = $results->fetch_object()) {
     $output .= "<li><a href='./edit_movie.php?id=$movie->id' title='$movie->name page'>$movie->name</a></li>";
