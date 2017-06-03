96b513937da447c8ee2e13d6b79cb3b82be30b1a
Shahar polak <shaharp@breezometer.com>
2017-06-03 14:15:06 +0300

    Added add movie button

--- php/movie_website/admin/index.php
+++ php/movie_website/admin/index.php
@@ -2,9 +2,10 @@
 
 include "../connection.php";
 include "../functions.php";
-$results = $mysqli->query("SELECT * FROM movies ORDER BY year DESC");
+$results = $mysqli->query("SELECT * FROM movies ORDER BY id DESC ");
 
 $output = "<h1>Movies Admin</h1>";
+$output.="<p><a href='add_movie.php'>+ Add a new movie</a></p>";
 
 while ($movie = $results->fetch_object()) {
     $output .= "<li><a href='./edit_movie.php?id=$movie->id' title='$movie->name page'>$movie->name</a></li>";
