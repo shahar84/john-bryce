8da2266169d1f0b67850158be6a293ee9140c4f8
Shahar polak <shaharp@breezometer.com>
2017-06-03 14:49:24 +0300

    Added some bootstrap style to add movie button

--- php/movie_website/admin/index.php
+++ php/movie_website/admin/index.php
@@ -5,7 +5,7 @@ include "../functions.php";
 $results = $mysqli->query("SELECT * FROM movies ORDER BY id DESC ");
 
 $output = "<h1>Movies Admin</h1>";
-$output.="<p><a href='add_movie.php'>+ Add a new movie</a></p>";
+$output.="<p><a href='add_movie.php' class='btn btn-default'>+ Add a new movie</a></p>";
 
 while ($movie = $results->fetch_object()) {
     $output .= "<li><a href='./edit_movie.php?id=$movie->id' title='$movie->name page'>$movie->name</a></li>";
